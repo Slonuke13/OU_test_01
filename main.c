9fdc5dc6a861e0fc000ea655c46379a82da28cc2
Slonuke13 <251221@edu.p.lodz.pl>
2023-10-26 15:14:43 +0200

    Nowa metoda Policz

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 
 
 int Policz(int a,int b){
-    return a + b;
+    return a * b;
 }
 int main() {
     printf("Hello, World!\n");
-    printf("W pierwszym branchu 2 + 3 = %d",Policz(2,3));
+    printf("W pierwszym branchu 2 * 3 = %d",Policz(2,3));
     return 0;
 }
